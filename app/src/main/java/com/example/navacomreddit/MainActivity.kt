112612cca8d093c940d8358d031a2c81ef089c05
Okabe94 <33587455+Okabe94@users.noreply.github.com>
2019-02-08 20:04:06 -0500

    First view recolecting Json with API
    The app is currently fetching the data needed for it's functions and being displayed.

--- app/src/main/java/com/example/navacomreddit/MainActivity.kt
+++ app/src/main/java/com/example/navacomreddit/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.navacomreddit
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView_MainPage.layoutManager = LinearLayoutManager(this)
-        recyclerView_MainPage.adapter = PostAdapter()
 
         fetchPosts()
     }
@@ -28,10 +33,20 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body()?.string()
-                println("HEY WE WORKING")
-                println(body)
+                val gson = GsonBuilder().create()
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+                runOnUiThread(){
+                    recyclerView_MainPage.adapter = PostAdapter(homeFeed)
+                }
             }
         })
 
     }
 }
+
+//Json Abstraction
+class HomeFeed(val data: Data)
+class Data(val children: List<Children>)
+class Children(val data: ChildrenData)
+class ChildrenData(val banner_img: String, val public_description: String, val community_icon: String,
+      val subscribers: Long, val url: String, val created_utc: Long)
--- app/src/main/java/com/example/navacomreddit/Reddit.kt
+++ app/src/main/java/com/example/navacomreddit/Reddit.kt
@@ -4,24 +4,35 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.post_row.view.*
+import java.text.DateFormat
+import java.text.NumberFormat
+import java.util.*
 
-class PostAdapter: RecyclerView.Adapter<ViewHolder>() {
+class PostAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<ViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 15
+        return homeFeed.data.children.size
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(p0.context)
-        val cellForRow = layoutInflater.inflate(R.layout.post_row, p0,false)
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.post_row, parent,false)
         return ViewHolder(cellForRow)
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = homeFeed.data.children.get(position)
+        holder.view.textView_Name.text = post.data.url
+        holder.view.textView_Description.text = post.data.public_description
+        holder.view.textView_Subscribers.text = NumberFormat.getInstance().format(post.data.subscribers)
+        val utc = Date(post.data.created_utc)
+        val date = DateFormat.getDateInstance(DateFormat.LONG).format(utc)
+        holder.view.textView_CreationDate.text = date.toString()
     }
 
 }
 
-class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
 
 }
